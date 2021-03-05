18654bc9d67960e25f121a03569132bfddf5e12a
Willer Pulis <willerpulis@ciandt.com>
2021-03-05 10:52:27 -0300

    some adjustments...

--- boto3/cwlogs_set_log_group_retention.py
+++ boto3/cwlogs_set_log_group_retention.py
@@ -11,11 +11,17 @@
 import boto3
 import os
 
+# Array of non-compliant CloudWatch Logs log groups
 nonCompliantLogGroups = []
+
+# Getting the AWS region from environment variables
 awsRegion = os.environ['awsRegion']
+
+# Creating the CloudWatch Logs client
 cwlclient = boto3.client('logs', region_name=awsRegion)
 
-def evaluate(cwlgroups):
+# This function checks if the log groups have the correct retention period set
+def evaluate(cwlgroups, retentionInDays):
     
     for group in cwlgroups:
         
@@ -25,10 +31,12 @@ def evaluate(cwlgroups):
             grpret = group['retentionInDays']
         except KeyError:
             grpret = 0
-            
-        if grpret != 30:
+        
+        # Adds the log group to the non-compliant array if the retention is not correct
+        if grpret != retentionInDays:
             nonCompliantLogGroups.append(grpname)
 
+# This function sets the retention of non-compliant groups based on the retentionInDays environment variable
 def remediate(nonCompliantLogGroups,retentionInDays):
     
     for group in nonCompliantLogGroups:
@@ -42,14 +50,16 @@ def lambda_handler(event, context):
     retentionInDays = int(os.environ['retentionInDays'])
     
     cwlgroups = cwlclient.describe_log_groups()
-    evaluate(cwlgroups['logGroups'])
+    evaluate(cwlgroups['logGroups'],retentionInDays)
     
     if 'nextToken' in cwlgroups:
         while True:
             cwlgroups = cwlclient.describe_log_groups(nextToken=cwlgroups['nextToken'])
-            evaluate(cwlgroups['logGroups'])
+            evaluate(cwlgroups['logGroups'],retentionInDays)
             if not 'nextToken' in cwlgroups:
                 break
     
     print("Number of log groups to be remediated: " + str(len(nonCompliantLogGroups)))
     remediate(nonCompliantLogGroups,retentionInDays)
+    
+    return "Complete."
